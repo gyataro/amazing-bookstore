d35c508430d137235ffe0c695aa2539393f8dcf7
yataro <teoh6g@gmail.com>
2021-03-08 00:35:57 +0800

    Update error page design and layout

--- frontend/src/components/pages/ErrorPage.js
+++ frontend/src/components/pages/ErrorPage.js
@@ -1,31 +1,34 @@
 import React from "react"
 import {
     Flex,
-    Box,
-    Link,
-    Button,
     Heading,
     Text,
-    Image,
     Stack,
-    Badge
+    Badge,
+    Box
 } from '@chakra-ui/react';
+import {
+    Link
+} from 'react-router-dom';
 import Header from "../sections/Header";
 
 export default function ErrorPage() {
     return (
-        <Flex minH={'100vh'} align={'center'} justify={'center'} bg={'gray.50'}>
-            <Stack align={'center'} spacing={8} mx={'auto'} maxW={'80%'} py={12} px={6}>
-                <Heading fontSize={'6xl'}>:(</Heading>
-                <Heading fontSize={'4xl'}>
-                    Sorry, we couldn't find that page.&nbsp;
-                    <Badge variant="subtle" fontSize={"4xl"} colorScheme="red">404</Badge>
-                </Heading>
-                <Text fontSize={'lg'} color={'gray.600'}>
-                    Go back to <Link color={'blue.400'}>homepage</Link>
-                </Text>
+        <Box minH={'100vh'} align={'center'} justify={'center'} bg={'gray.50'}>
+            <Header />
+            <Flex mt={[8, 8, 40, 40]}>
+                <Stack align={'center'} spacing={8} mx={'auto'} maxW={'80%'} py={12} px={6}>
+                    <Heading fontSize={'6xl'}>:(</Heading>
+                    <Heading fontSize={'4xl'}>
+                        Sorry, we couldn't find that page.&nbsp;
+                        <Badge variant="subtle" fontSize={"4xl"} colorScheme="red">404</Badge>
+                    </Heading>
+                    <Text fontSize={'lg'} color={'gray.600'}>
+                        Go back to <Link to={'/'} style={{ color: '#00B5D8' }}>homepage</Link>
+                    </Text>
 
-            </Stack>
-        </Flex>
+                </Stack>
+            </Flex>
+        </Box>
     );
 }
