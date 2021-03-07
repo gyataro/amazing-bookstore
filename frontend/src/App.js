abb3525539b8cd4530ef28b92e5141b33c744484
yataro <teoh6g@gmail.com>
2021-03-08 00:41:47 +0800

    Update React router paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,11 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LandingPage from './components/pages/LandingPage.js';
 import LoginPage from './components/pages/LoginPage.js';
 import RegisterPage from './components/pages/RegisterPage.js';
+import SearchPage from './components/pages/SearchPage.js';
 import ErrorPage from './components/pages/ErrorPage.js';
+import AdminPage from './components/pages/AdminPage.js';
+import UserPage from './components/pages/UserPage.js';
+import BookPage from './components/pages/BookPage.js';
 
 function App() {
     return (
@@ -17,6 +21,18 @@ function App() {
                 </Route>
                 <Route exact path="/register">
                     <RegisterPage />
+                </Route>
+                <Route exact path="/search">
+                    <SearchPage />
+                </Route>
+                <Route exact path="/admin">
+                    <AdminPage />
+                </Route>
+                <Route exact path="/user">
+                    <UserPage />
+                </Route>
+                <Route exact path="/book/:id">
+                    <BookPage />
                 </Route>
                 <Route path="*">
                     <ErrorPage />
